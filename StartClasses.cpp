98c9104bfdc4253da50a382b5d27741131249dae
bondha1 <haibond818@gmail.com>
2025-11-07 14:31:46 +0400

    исправление StartClasses.cpp

--- StartClasses.cpp
+++ StartClasses.cpp
@@ -3,59 +3,57 @@ bool Warrior::Save()
 {
 
 
-    if (Npc::Save())
+    
+    ofstream saveSystem("save.bin", ios::binary);
+    if (saveSystem.is_open())
     {
-        ofstream saveSystem("save.bin", ios::binary);
-        if (saveSystem.is_open())
-        {
-
-            saveSystem.write(reinterpret_cast<const char*>(&strenght), sizeof(strenght));
-            for (int i = 0; i < 4; i++)
-            {
-                saveSystem.write(reinterpret_cast<const char*>(&weapons[i]), sizeof(weapons[i]));
-            }
-            saveSystem.close();
-            return true;
-        }
-        else
+        if (Npc::Save())
         {
             cout << "сохранение не удалось" << endl;
             return false;
         }
+        saveSystem.write(reinterpret_cast<const char*>(&strenght), sizeof(strenght));
+        saveSystem.close();
+        return true;
     }
+    else
+    {
+        cout << "сохранение не удалось" << endl;
+        return false;
+    }
+    
 };
-Warrior Warrior::Load()
+bool Warrior::Load()
 {
     ifstream loadSystem("save.bin", ios::binary);
-    Warrior warrior; //временное хранилище для считывания данных из файла
-    warrior = Npc::Load();
+   
     if (loadSystem.is_open())
     {
-        loadSystem.read(reinterpret_cast<char*>(&strenght), sizeof(strenght));
-        for (int i = 0; i < 4; i++)
+        if (!Npc::Load())
         {
-            loadSystem.read(reinterpret_cast<char*>(&weapons[i]), sizeof(weapons[i]));
+            cout << "связь с базой нарушена\nПамять утерена" << endl;
+            return false;
         }
+        loadSystem.read(reinterpret_cast<char*>(&strenght), sizeof(strenght));
+       
     }
     else
     {
         cout << "связь с базой нарушена\nПамять утерена" << endl;
-        return warrior;
+        return false;
     }
     loadSystem.close();
-    return warrior;
+    return true;
 
 
 };
-
-
 Warrior::Warrior() //конструктор по умолчанию, когда нет аргументов
 {
     name = "воин";
     health = 35;
     damage = 10;
 }
-//кастомный конструктор
+
 Warrior::Warrior(string name, unsigned int health, float damage)
 {
     cout << "кастомный конструктор война" << endl;
@@ -88,20 +86,89 @@ void Warrior::Create()
 }
 bool Warrior::operator == (const Warrior& warrior) const
 {
-    return ((warrior.damage == this->damage) && (warrior.health == this->health)
-        && (warrior.strenght == this->strenght));
+    return (warrior.damage == this->damage) &&
+           (warrior.health == this->health) &&
+           (warrior.strenght == this->strenght);
 }
-void Warrior::operator = (Npc npc)
+void  Warrior::operator = (const Npc& npc)
+{
+    if (this != &npc)
+    {
+        this->name = npc.GetName();
+        this->health = npc.GetHealth();
+        this->damage = npc.GetDamage();
+        this->lvl = npc.GetLvl();
+        return *this;
+    }
+}
+
+
+Warrior::~Warrior() //деструктор всегда без аргументов
+{
+    cout << name << " пал смертью храбрых" << endl;
+}
+
+   
+void Wizard::GetInfo() //полиморфизм (перегрузка для метода)
+{
+    Npc::GetInfo();
+    cout << "Интеллект - " << intellect << endl;
+    cout << "Доступные заклинания в книге заклинаний - ";
+    for (int i = 0; i < lvl; i++)
+    {
+        cout << spell[i] << endl;
+    }
+}
+void Wizard::CastSpell()
+{
+    cout << name << " применяет " << spell[lvl - 1] << endl;
+}
+void Wizard::Create()
+{
+    cout << "Вы создали волшебника" << endl;
+    cout << "Введите имя персонажа\t";
+    cin >> name;
+    GetInfo();
+    CastSpell();
+}
+bool Wizard::operator == (const Wizard& wizard) const
+{
+    return ((wizard.damage == this->damage) && (wizard.health == this->health)
+        && (wizard.intellect == this->intellect));
+}
+void Wizard::operator = (Npc npc)
 {
     this->name = npc.GetName();
     this->name = npc.GetHealth();
     this->name = npc.GetDamage();
     this->name = npc.GetLvl();
 }
-
-//деструктор - метод, который вызывается автоматически при высвобождении памяти
-//при окончании работы с экземпляром класса (нельзя вызвать вручную)
-Warrior::~Warrior() //деструктор всегда без аргументов
+bool Wizard::Save() 
 {
-    cout << name << " пал смертью храбрых" << endl;
+
+
+    if (Npc::Save())
+    {
+        ofstream saveSystem("save.bin", ios::binary);
+        if (saveSystem.is_open())
+        {
+
+            saveSystem.write(reinterpret_cast<const char*>(&intellect), sizeof(intellect));
+            for (int i = 0; i < 4; i++)
+            {
+                saveSystem.write(reinterpret_cast<const char*>(&spell[i]), sizeof(spell[i]));
+            }
+            saveSystem.close();
+            return true;
+        }
+        else
+        {
+            cout << "сохранение не удалось" << endl;
+            return false;
+        }
+    }
+}
+Wizard::~Wizard() //деструктор всегда без аргументов
+{
+    cout << name << " испустил дух" << endl;
 }
